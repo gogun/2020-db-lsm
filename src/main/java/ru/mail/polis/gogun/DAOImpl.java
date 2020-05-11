1e75f8c5569bb0aaa61024d5056b0d4980c880a8
Ilya <gusarov2000@gmail.com>
2020-05-11 15:30:05 +0300

    another codeclimate fix

--- src/main/java/ru/mail/polis/gogun/DAOImpl.java
+++ src/main/java/ru/mail/polis/gogun/DAOImpl.java
@@ -12,7 +12,6 @@ import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
